50561626073cafa97fdec0c11002288e3c17753f
BuildTools <unconfigured@null.spigotmc.org>
2020-08-10 23:19:35 +0800

    - Minor Bug Fix - Can now debug up to two digit hexadecimal values

--- main.py
+++ main.py
@@ -1,23 +1,35 @@
-number = 15
-
+number = 200
 def DataOf16Bit(number):
     anslist = []
-    hexdictionary = {'f':15,'e':14,'d':13,'c':12,'b':11,'a':10}
-    while number != 0:
-        for hexletter, value in hexdictionary.items():
-            remainder = number % value
-            solution = number / value
-            if solution >= 1:
+    hexval = ''
+    hexint = 0
+    hexdictionary = {1:16,'f':15,'e':14,'d':13,'c':12,'b':11,'a':10}
+
+    for hexletter, value in hexdictionary.items():
+
+        while number >= 16:              #specific condition needed for non-repetitive 1s
+            if anslist == []:
                 anslist.append(hexletter)
+                hexint += hexletter
                 number -= value
-            elif number == 0:
-                break
             else:
-                anslist.append(number)
-                number -= number
-            
+                anslist[0] = hexint + 1
+                hexint += 1
+                number -= value
+
+        if number >= 10 and value == number:
+            anslist.append(hexletter)
+            number -= value
+
+        elif number < 10:
+            anslist.append(number)
+            number -= number
+
+    for i in anslist:
+        if i != 0:
+            hexval += str(i)
     
-    return anslist
+    return hexval
 
 print(DataOf16Bit(number))
 
