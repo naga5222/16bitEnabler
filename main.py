3bf9f7ea36200f062bf35d9f080ef07b221de2d3
BuildTools <unconfigured@null.spigotmc.org>
2020-08-11 18:57:00 +0800

    data csv file added for testing purposes

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-number = 200
-def DataOf16Bit(number):
+number = 235
+
+def Calc16Bit(number):
     anslist = []
-    hexval = ''
     hexint = 0
-    hexdictionary = {1:16,'f':15,'e':14,'d':13,'c':12,'b':11,'a':10}
+    hexdictionary = {1:16,'F':15,'E':14,'D':13,'C':12,'B':11,'A':10}
 
     for hexletter, value in hexdictionary.items():
 
@@ -21,15 +21,33 @@ def DataOf16Bit(number):
             anslist.append(hexletter)
             number -= value
 
-        elif number < 10:
+        elif number < 10 and number != 0:
             anslist.append(number)
             number -= number
 
-    for i in anslist:
-        if i != 0:
-            hexval += str(i)
+        else:
+            pass
     
+    return anslist
+
+def DataOf16Bit(number):
+    hexdictionary = {1:16,'f':15,'e':14,'d':13,'c':12,'b':11,'a':10}
+    hexval = ''
+
+    ans = Calc16Bit(number)
+    print(ans)
+    for i in ans:
+
+        if i >= 10:
+            ansactual = Calc16Bit(i)
+            for j in ansactual:
+                hexval += str(j)
+
+        elif i != 0:
+            hexval += str(i)
+
     return hexval
+
 
 print(DataOf16Bit(number))
 
